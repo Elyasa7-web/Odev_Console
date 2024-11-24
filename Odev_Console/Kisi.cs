a83a14983ee00e5128a9536f3870b4c04c1bb7c1
fb_20 <fb_20@ELYASA>
2024-11-24 14:04:15 +0300

    Add project files.

---
+++ Odev_Console/Kisi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_Console
+{
+    internal class Kisi
+    {
+        public void kisi(string adsoyad,string meslek,int yasi)
+        {
+            Console.WriteLine(adsoyad);
+            Console.WriteLine(meslek);
+            Console.WriteLine(yasi);
+        }
+    }
+}
---
+++ Odev_Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi k = new Kisi();
+            string ads;
+            Console.Write("İsim Girin: ");
+            ads = Console.ReadLine();
+            string meslek;
+            Console.Write("Mesleğinizi Girin: ");
+            meslek=Console.ReadLine();
+            Console.Write("Yaşınızı Girin: ");
+            int yasi=Convert.ToInt32(Console.ReadLine());
+            k.kisi(ads, meslek, yasi);
+            Console.Read();
+        }
+    }
+}
